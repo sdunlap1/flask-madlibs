631bdff91bce39d1f78552aff18faef437778233
sdunlap1 <sdunlap1@mac.com>
2024-04-19 20:44:22 -0700

    Adding app.py, static folder with .css file and templates folder with form.html and story.html

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect, flash, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from random import randint, choice, sample
+from stories import story
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "IMMMA"
@@ -9,6 +10,13 @@ app.config["DEBUG"] = True
 debug = DebugToolbarExtension(app)
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False #or True
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
-  return "IT'S WORKING YO!"
+  prompts = story.prompts
+  return render_template("form.html", prompts=prompts)
+
+@app.route("/story", methods=["POST"])
+def show_story():
+  answers = {prompt: request.form[prompt] for prompt in story.prompts}
+  result_story = story.generate(answers)
+  return render_template("story.html", story=result_story)
